01eae5b1cd2d90f833a7f0c871fd0c627c708147
soul <miya@miya.local>
2016-07-19 17:14:34 +0800

    Initial Commit

---
+++ TomCat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TomCat
+//
+//  Created by miya on 16/7/19.
+//  Copyright © 2016年 miya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TomCat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TomCat
+//
+//  Created by miya on 16/7/19.
+//  Copyright © 2016年 miya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
